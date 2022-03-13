31c003251afc12ab9fed7c233b985d3806463084
CronobotTech <shahsohaib09@gmail.com>
2022-03-13 23:08:07 +0400

    added fonts locally

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import './fonts/Lato/Lato-Regular.ttf'
+import './fonts/Lato/Lato-Black.ttf'
+import './fonts/Lato/Lato-Bold.ttf'
+
 import './index.css'
 
 import App from './App'
